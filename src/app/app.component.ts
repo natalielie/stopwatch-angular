0dc16f9a7ce1273e267ca0fb296f38f8ecaaa6e3
Наталія Малищенко <nataliia.malyshchenko@nure.ua>
2023-10-17 15:14:30 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from "@angular/core";
+import { Subject } from "rxjs";
+import { StopWatch } from "./stopwatch.interface";
+import { StopwatchService } from "./stopwatch.service";
 
+/**
+ * Stopwatch component which uses StopwatchService 
+ * for implementing stopwatch functionality
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'stopwatch-angular';
+export class AppComponent implements OnDestroy {
+  // a stopwatch property of Stopwatch interface
+  public stopwatch!: StopWatch;
+  // a subject that emits a truthy value in the ngOnDestroy lifecycle hook
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private stopwatchService: StopwatchService) {
+      this.stopwatchService.stopWatch$.subscribe(
+        (val: StopWatch) => (this.stopwatch = val)
+      )
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  public startCount(): void {
+    this.stopwatchService.startCount();
+  }
+
+  public stopCount(): void {
+    this.stopwatchService.stopCount();
+  }
+
+  public resetStopwatch(): void {
+    this.stopwatchService.resetStopwatch();
+  }
+  public waitCount(): void {
+    this.stopwatchService.waitCount();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from '@angular/material/button';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    BrowserModule, 
+    FormsModule,
+    MatButtonModule],
+
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
