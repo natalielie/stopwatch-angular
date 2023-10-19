fd0672939a8df003f9367ed7e9776e4e2225932c
Наталія Малищенко <nataliia.malyshchenko@nure.ua>
2023-10-19 13:31:50 +0300

    Changed display time to pipe, minimized variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
-import { StopWatch } from './stopwatch.interface';
-import { StopwatchService } from './stopwatch.service';
+import {
+  Subject,
+  Subscription,
+  buffer,
+  debounceTime,
+  filter,
+  fromEvent,
+  map,
+  takeUntil,
+  timer,
+} from 'rxjs';
 
 /**
  * Stopwatch component which uses StopwatchService
@@ -13,18 +21,13 @@ import { StopwatchService } from './stopwatch.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
-  [x: string]: any;
   // a stopwatch property of Stopwatch interface
-  public stopwatch!: StopWatch;
-  // a subject that emits a truthy value in the ngOnDestroy lifecycle hook
-  destroy$: Subject<boolean> = new Subject<boolean>();
+  public stopwatch: number = -new Date().getTimezoneOffset();
   isRunning: boolean = false;
 
-  constructor(private stopwatchService: StopwatchService) {
-    this.stopwatchService.stopWatch$.subscribe(
-      (val: StopWatch) => (this.stopwatch = val)
-    );
-  }
+  #timerSubscription: Subscription = new Subscription();
+  // a subject that emits a truthy value in the ngOnDestroy lifecycle hook
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   ngOnDestroy() {
     this.destroy$.next(true);
@@ -35,23 +38,53 @@ export class AppComponent implements OnDestroy {
    * Starts the stopwatch from the initial time or the last stopped count
    */
   public handleCount(): void {
-    this.stopwatchService.handleCount();
-    this.isRunning = this.stopwatchService.isRunning;
+    //if the time is running, stop the count
+    if (this.isRunning) {
+      this.destroy$.next(true);
+      this.#timerSubscription.unsubscribe();
+      this.isRunning = false;
+      return;
+    }
+    // timer for emitting each value every second
+    this.#timerSubscription = timer(0, 1000)
+      .pipe(
+        map(() => (this.stopwatch += 1000)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => {});
+
+    this.isRunning = true;
   }
 
   /**
    * Resets the stopwatch to 0
    */
   public resetStopwatch(): void {
-    this.stopwatchService.resetStopwatch();
+    this.destroy$.next(true);
+    this.#timerSubscription.unsubscribe();
+    this.stopwatch = 0;
     this.isRunning = false;
   }
 
   /**
    * Checks if there were two consecutive clicks within 300ms and stops the time
+   *
+   * @param mouse$ check if there was a click
+   * @param buff$ wait 300ms before emission
+   * @param click$ check if there were 2 clicks
+   *
    */
   public waitCount(): void {
-    this.stopwatchService.waitCount();
-    this.isRunning = false;
+    const mouse$ = fromEvent(document, 'click');
+    const buff$ = mouse$.pipe(debounceTime(300));
+    const click$ = mouse$.pipe(
+      buffer(buff$),
+      map((list) => {
+        return list.length;
+      }),
+      filter((x) => x === 2),
+      takeUntil(this.destroy$)
+    );
+    click$.subscribe(() => this.handleCount());
   }
 }
--- src/app/stopwatch.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface StopWatch {
-  seconds: string;
-  minutes: string;
-  hours: string;
-}
